56a72b8fcb5e80a9a96f23c76f6f29f6f7efab36
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2022-03-22 16:41:47 +0200

    correct get

--- src/main/kotlin/fi/johannes/App.kt
+++ src/main/kotlin/fi/johannes/App.kt
@@ -4,9 +4,9 @@ package fi.johannes
  * Johannes on 21.3.2022.
  */
 
-val getProperty = { key: String, default: String -> System.getProperty(key) ?: default }
+val getEnv = { key: String, default: String -> System.getenv(key) ?: default }
 fun main(args: Array<String>) {
-    val bootstrap = getProperty("KAFKA_BOOTSTRAP", "kafka-bootstrap:9092");
-    val schemaRegistry = getProperty("SCHEMA_REGISTRY", "schema-registry:8081");
+    val bootstrap = getEnv("KAFKA_BOOTSTRAP", "kafka-bootstrap:9092");
+    val schemaRegistry = getEnv("SCHEMA_REGISTRY", "schema-registry:8081");
     SimpleProducer(bootstrap, schemaRegistry).produce(5)
 }
